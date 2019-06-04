603f371e773f28b41ed6db84fe1adbd91c39b005
Scott Bowler <scott.bowler@learningfuze.com>
2019-06-03 23:53:58 -0700

    Completed php-intro prototype

---
+++ php-intro/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>PHP Intro - Home</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="about.php">About</a>
+                </li>
+                <li>
+                    <a href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="main">
+            <h1 class="center">Welcome to PHP</h1>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam obcaecati eius iste necessitatibus aliquam quo!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ratione obcaecati quisquam delectus placeat, vero aspernatur ipsa, deserunt neque facilis quibusdam asperiores corrupti, et eos.</p>
+        </div>
+
+        <div class="logo center">
+            <img src="assets/images/php_logo.png" alt="PHP Logo">
+        </div>
+
+        <footer class="center">
+            &copy; Copyright 1995
+        </footer>
+    </div>
+</body>
+</html>
